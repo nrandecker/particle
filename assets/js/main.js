35b76dd227526641335dcfbcd2ef18cd9d9abc1e
Nathan Randecker <nathan.randecker@gmail.com>
2017-01-16 00:11:45 -0600

    Added some styles, particle.js

---
+++ assets/js/main.js
@@ -0,0 +1,1 @@
+particlesJS.load("particles-js","assets/particles.json",function(){console.log("callback - particles-js config loaded")});
---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles-js config loaded');
+});
